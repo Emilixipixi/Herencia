86e3106dcf6a02cfced47c1c87a98b3404e91538
Emilixipixi <carlosemilio270798@gmail.com>
2026-05-09 00:03:02 -0500

    Herencias completo

---
+++ src/com/krakedev/herencia/test/TestHija.java
@@ -0,0 +1,18 @@
+package com.krakedev.herencia.test;
+
+import com.krakedev.herencia.Hija;
+
+public class TestHija {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Hija hija= new Hija();
+		
+		hija.escucharBadBunny();
+		
+		
+
+	}
+
+}
